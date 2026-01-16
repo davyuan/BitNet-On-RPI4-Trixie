9591e89d22a5553dbd105d31f57e7a5b35eb6e6c
David Yuan <davyuan@gmail.com>
2026-01-16 13:52:15 -0600

    update

--- test/matmul_cortex72.cpp
+++ test/matmul_cortex72.cpp
@@ -8,8 +8,9 @@
 #include <omp.h>
 #include <arm_neon.h>
 
-#define TILE_K 16
+#define TILE_K 32
 #define TILE_N 16
+#define TILE_M 4; // Register block height
 #define TILE_SIZE 16  // Tile size for blocking
 const int M  = 640;           // Activation rows (B rows)
 const int K  = 2560;        // Shared dimension
@@ -33,6 +34,71 @@ static void aligned_free(void * ptr) {
 #endif
 }
 
+void matmul_tiled_simd(int8_t* A, int8_t* B, int32_t* C, int M, int N, int K) {
+    #pragma omp parallel for num_threads(4)
+    for (int i = 0; i < M; i += TILE_M) {
+        for (int j = 0; j < N; j += TILE_N) {            
+            // 1. Initialize 4x4 registers (total 16 accumulators)
+            // Each register holds four 32-bit results
+            // Row 0 Accumulators (16 elements total)
+            int32x4_t c00 = vdupq_n_s32(0);
+            int32x4_t c01 = vdupq_n_s32(0);
+            int32x4_t c02 = vdupq_n_s32(0);
+            int32x4_t c03 = vdupq_n_s32(0);
+
+            // Row 1 Accumulators
+            int32x4_t c10 = vdupq_n_s32(0);
+            int32x4_t c11 = vdupq_n_s32(0);
+            int32x4_t c12 = vdupq_n_s32(0);
+            int32x4_t c13 = vdupq_n_s32(0);
+
+            // Row 2 Accumulators
+            int32x4_t c20 = vdupq_n_s32(0);
+            int32x4_t c21 = vdupq_n_s32(0);
+            int32x4_t c22 = vdupq_n_s32(0);
+            int32x4_t c23 = vdupq_n_s32(0);
+
+            // Row 3 Accumulators
+            int32x4_t c30 = vdupq_n_s32(0);
+            int32x4_t c31 = vdupq_n_s32(0);
+            int32x4_t c32 = vdupq_n_s32(0);
+            int32x4_t c33 = vdupq_n_s32(0);
+
+            for (int k = 0; k < K; k++) {
+                // 2. Load 16 elements from B once
+                int8x16_t vb = vld1q_s8(&B[k * N + j]);
+
+                // 3. Process 4 rows of A for this k
+                // We broadcast A[i][k], A[i+1][k]... and multiply by the same vb
+                #define COMPUTE_ROW(r, acc0, acc1, acc2, acc3) { \
+                    int8x16_t va = vdupq_n_s8(A[(i+r) * K + k]); \
+                    int16x8_t prod_l = vmull_s8(vget_low_s8(va), vget_low_s8(vb)); \
+                    int16x8_t prod_h = vmull_s8(vget_high_s8(va), vget_high_s8(vb)); \
+                    acc0 = vaddw_s16(acc0, vget_low_s16(prod_l)); \
+                    acc1 = vaddw_s16(acc1, vget_high_s16(prod_l)); \
+                    acc2 = vaddw_s16(acc2, vget_low_s16(prod_h)); \
+                    acc3 = vaddw_s16(acc3, vget_high_s16(prod_h)); \
+                }
+
+                COMPUTE_ROW(0, c00, c01, c02, c03);
+                COMPUTE_ROW(1, c10, c11, c12, c13);
+                COMPUTE_ROW(2, c20, c21, c22, c23);
+                COMPUTE_ROW(3, c30, c31, c32, c33);
+            }
+
+            // 4. Store the 4x16 block back to Matrix C
+            vst1q_s32(&C[(i+0)*N+j+0], c00); vst1q_s32(&C[(i+0)*N+j+4], c01);
+            vst1q_s32(&C[(i+0)*N+j+8], c02); vst1q_s32(&C[(i+0)*N+j+12], c03);
+            vst1q_s32(&C[(i+1)*N+j+0], c10); vst1q_s32(&C[(i+1)*N+j+4], c11);
+            vst1q_s32(&C[(i+1)*N+j+8], c12); vst1q_s32(&C[(i+1)*N+j+12], c13);
+            vst1q_s32(&C[(i+2)*N+j+0], c20); vst1q_s32(&C[(i+2)*N+j+4], c21);
+            vst1q_s32(&C[(i+2)*N+j+8], c22); vst1q_s32(&C[(i+2)*N+j+12], c23);
+            vst1q_s32(&C[(i+3)*N+j+0], c30); vst1q_s32(&C[(i+3)*N+j+4], c31);
+            vst1q_s32(&C[(i+3)*N+j+8], c32); vst1q_s32(&C[(i+3)*N+j+12], c33);
+        }
+    }
+}
+/*
 void matmul_tiled_simd(int8_t* A, int8_t* B, int32_t* C, int M, int N, int K) {
     // 1. Parallelize over rows (Each core gets a private slice of M)
 #pragma omp parallel for num_threads(4)
@@ -82,7 +148,7 @@ void matmul_tiled_simd(int8_t* A, int8_t* B, int32_t* C, int M, int N, int K) {
             }
         }
     }
-}
+}*/
 
 void matmul_naive(int8_t* A, int8_t* B, int32_t* C, int M, int N, int K) {
     for (int i = 0; i < M; i++) {
@@ -101,13 +167,11 @@ void matmul_int8(int8_t* A, int8_t* B, int32_t* C, int M, int N, int K) {
     #pragma omp parallel for num_threads(4) 
     for (int ii = 0; ii < M; ii += TILE_SIZE) {          
         for (int jj = 0; jj < N; jj += TILE_SIZE) {      
-            for (int kk = 0; kk < K; kk += TILE_SIZE) {
-                
+            for (int kk = 0; kk < K; kk += TILE_SIZE) {                
                 // Micro-kernel: This part would be replaced by NEON intrinsics 
                 // for actual production speed.
                 for (int i = ii; i < std::min(ii + TILE_SIZE, M); i++) {
-                    for (int j = jj; j < std::min(jj + TILE_SIZE, N); j++) {
-                        
+                    for (int j = jj; j < std::min(jj + TILE_SIZE, N); j++) {                        
                         // Local accumulator in a CPU register
                         int32_t local_sum = 0; 
                         
