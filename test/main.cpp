8886484ec05d3547f9175591ecced8e0419ba2fc
David Yuan <davyuan@gmail.com>
2026-01-17 12:54:47 -0600

    update

--- test/main.cpp
+++ test/main.cpp
@@ -123,7 +123,7 @@ int main() {
     // Allocate matrices
     float32_t* B = (float32_t*)aligned_malloc(N * K * sizeof(float32_t));
     float32_t* B_T = (float32_t*)aligned_malloc(N * K * sizeof(float32_t));
-    uint8_t* A = (uint8_t*)aligned_malloc(M * K / 2 * sizeof(uint8_t));
+    int8_t* A = (int8_t*)aligned_malloc(M * K / 2 * sizeof(int8_t));
     int8_t* A_ = (int8_t*)aligned_malloc(M * K * sizeof(int8_t));
     uint8_t* A_packed = (uint8_t*)aligned_malloc(M * K / 4 * sizeof(uint8_t));
     int32_t* C = (int32_t*)aligned_malloc(M * N * sizeof(int32_t));
