e5006563ed3ac44fabf76be4c48141a1f7c6552f
David Yuan <davyuan@gmail.com>
2026-01-17 15:29:19 -0600

    update

--- test/main.cpp
+++ test/main.cpp
@@ -9,15 +9,15 @@
 #define TILE_N 16
 #define TILE_M 4
 #define TILE_SIZE 2
-#define K_DIM 32
+#define K_DIM 64
 
 const int BM = 160;
 const int BY = 256;
 const int bm = 32;
 const int by = (256/(bm));
-const int M =16;           // Activation rows (B rows)
-const int K = 32;        // Shared dimension
-const int N = 16;         // Weight rows (A rows) = output size
+const int M =64;           // Activation rows (B rows)
+const int K = 64;        // Shared dimension
+const int N = 64;         // Weight rows (A rows) = output size
 
 // Repack matrix A according to the tl1 layout pattern
 // BM, BY, bm, by are the tiling parameters
