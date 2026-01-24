ee5e6282b3b7864a37f4f378d7566d1ab1f7f75d
David Yuan <davyuan@gmail.com>
2026-01-23 21:54:03 -0600

    update

--- utils/convert-hf-to-gguf-bitnet.py
+++ utils/convert-hf-to-gguf-bitnet.py
@@ -966,10 +966,9 @@ class BitnetModel(Model):
                 tokens.append(f"[PAD{i}]")
                 toktypes.append(gguf.TokenType.USER_DEFINED)
             elif reverse_vocab[i] in added_vocab:
-                # We need to manually encode and decode the added tokens in case special characters
-                # used for `\n` / `\t` have been manually added in the added tokens
-                encoded_decoded_token = tokenizer.decode(tokenizer.encode(reverse_vocab[i]))
-                tokens.append(encoded_decoded_token)
+                # For special tokens, use the raw token text directly
+                # Don't encode/decode as it can cause merging of token sequences
+                tokens.append(reverse_vocab[i])
                 if tokenizer.added_tokens_decoder[i].special:
                     toktypes.append(gguf.TokenType.CONTROL)
                 else:
