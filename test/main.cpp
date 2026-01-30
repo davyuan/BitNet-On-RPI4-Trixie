3b04eab48a9481ead7a03fd246826199f271e0cf
David Yuan <davyuan@gmail.com>
2026-01-29 23:20:51 -0600

    update

--- test/main.cpp
+++ test/main.cpp
@@ -470,71 +470,109 @@ void matmul_lut_simd2(uint8_t* A, float32_t* B, float32_t* C, int M, int N, int 
 */
 void matmul_lut_packed(uint8_t* A, float32_t* B, float32_t* C, float32_t* ws, int M, int N, int K) {
     int KK = K / 2;
-    int8_t* QLUT = (int8_t*)aligned_malloc(K * 16 * sizeof(int8_t));    
+    int8_t* QLUT0 = (int8_t*)aligned_malloc(K * 16 * sizeof(int8_t));    
+    int8_t* QLUT1 = (int8_t*)aligned_malloc(K * 16 * sizeof(int8_t));    
     const uint8x16_t vec_mask = vdupq_n_u8(0x0f);
-    float32_t* LUT_Scales = (float32_t*)aligned_malloc(sizeof(float32_t));
+    float32_t* LUT_Scales = (float32_t*)aligned_malloc(2 * sizeof(float32_t));
     const float32_t weight_scale = ws[0];
 
-    for (int j = 0; j < N; j++) {
-        ggml_preprocessor(M, K, (void*)(B + j * K), (void*)LUT_Scales, (void*)QLUT);                  
-        const float32_t lut_scale = ((float32_t*)LUT_Scales)[0];
-        const float32x4_t v_rescale = vdupq_n_f32(weight_scale / lut_scale);
+    for (int j = 0; j < N; j += 2) {
+        ggml_preprocessor(M, K, (void*)(B + j * K), (void*)(&LUT_Scales[0]), (void*)QLUT0);                  
+        ggml_preprocessor(M, K, (void*)(B + (j + 1) * K), (void*)(&LUT_Scales[1]), (void*)QLUT1);                  
+        
+        const float32x4_t v_rescale0 = vdupq_n_f32(weight_scale / LUT_Scales[0]);
+        const float32x4_t v_rescale1 = vdupq_n_f32(weight_scale / LUT_Scales[1]);
         
         // Parallelize over row blocks
         #pragma omp parallel for num_threads(4)
         for (int ii = 0; ii < M; ii += BM) {          
             for (int i = ii; i < ii + BM; i += 64) {
-                int16x8_t acc0 = vdupq_n_s16(0), acc1 = vdupq_n_s16(0);
-                int16x8_t acc2 = vdupq_n_s16(0), acc3 = vdupq_n_s16(0);
-                int16x8_t acc4 = vdupq_n_s16(0), acc5 = vdupq_n_s16(0);
-                int16x8_t acc6 = vdupq_n_s16(0), acc7 = vdupq_n_s16(0);
+                int16x8_t acc0_j0 = vdupq_n_s16(0), acc1_j0 = vdupq_n_s16(0);
+                int16x8_t acc2_j0 = vdupq_n_s16(0), acc3_j0 = vdupq_n_s16(0);
+                int16x8_t acc4_j0 = vdupq_n_s16(0), acc5_j0 = vdupq_n_s16(0);
+                int16x8_t acc6_j0 = vdupq_n_s16(0), acc7_j0 = vdupq_n_s16(0);
+
+                int16x8_t acc0_j1 = vdupq_n_s16(0), acc1_j1 = vdupq_n_s16(0);
+                int16x8_t acc2_j1 = vdupq_n_s16(0), acc3_j1 = vdupq_n_s16(0);
+                int16x8_t acc4_j1 = vdupq_n_s16(0), acc5_j1 = vdupq_n_s16(0);
+                int16x8_t acc6_j1 = vdupq_n_s16(0), acc7_j1 = vdupq_n_s16(0);
 
                 for (int k = 0; k < KK; k++) {
-                    int8x16_t vh = vld1q_s8(QLUT + k * 32);
-                    int8x16_t vl = vld1q_s8(QLUT + k * 32 + 16);
+                    int8x16_t vh0 = vld1q_s8(QLUT0 + k * 32);
+                    int8x16_t vl0 = vld1q_s8(QLUT0 + k * 32 + 16);
+                    int8x16_t vh1 = vld1q_s8(QLUT1 + k * 32);
+                    int8x16_t vl1 = vld1q_s8(QLUT1 + k * 32 + 16);
 
-                    // Process 64 rows (two 16-byte chunks from packed A)
                     int i_packed = i / 2;
                     
-#define PROCESS_32_ROWS(a_ptr, accl0, accl1, acch0, acch1) { \
+#define PROCESS_32_ROWS_2COL(a_ptr, acl0_j0, ach0_j0, acl1_j0, ach1_j0, \
+                                       acl0_j1, ach0_j1, acl1_j1, ach1_j1) { \
                         uint8x16_t vec_a = vld1q_u8(a_ptr); \
                         uint8x16_t vec_a_top = vshrq_n_u8(vec_a, 4); \
                         uint8x16_t vec_a_bot = vandq_u8(vec_a, vec_mask); \
-                        uint8x16x2_t vec_a_unpacked = vzipq_u8(vec_a_top, vec_a_bot); \
-                        int8x16_t r0h = vqtbl1q_s8(vh, vec_a_unpacked.val[0]); \
-                        int8x16_t r0l = vqtbl1q_s8(vl, vec_a_unpacked.val[0]); \
-                        int8x16_t r1h = vqtbl1q_s8(vh, vec_a_unpacked.val[1]); \
-                        int8x16_t r1l = vqtbl1q_s8(vl, vec_a_unpacked.val[1]); \
+                        uint8x16x2_t vec_a_unp = vzipq_u8(vec_a_top, vec_a_bot); \
+                        \
+                        /* Col j0 lookups */ \
+                        int8x16_t r0h_j0 = vqtbl1q_s8(vh0, vec_a_unp.val[0]); \
+                        int8x16_t r0l_j0 = vqtbl1q_s8(vl0, vec_a_unp.val[0]); \
+                        int8x16_t r1h_j0 = vqtbl1q_s8(vh0, vec_a_unp.val[1]); \
+                        int8x16_t r1l_j0 = vqtbl1q_s8(vl0, vec_a_unp.val[1]); \
+                        /* Col j1 lookups */ \
+                        int8x16_t r0h_j1 = vqtbl1q_s8(vh1, vec_a_unp.val[0]); \
+                        int8x16_t r0l_j1 = vqtbl1q_s8(vl1, vec_a_unp.val[0]); \
+                        int8x16_t r1h_j1 = vqtbl1q_s8(vh1, vec_a_unp.val[1]); \
+                        int8x16_t r1l_j1 = vqtbl1q_s8(vl1, vec_a_unp.val[1]); \
+                        \
                         int16x8_t o0, o1, o2, o3; \
-                        reconstruct_int16_pair(r0h, r0l, o0, o1); \
-                        reconstruct_int16_pair(r1h, r1l, o2, o3); \
-                        accl0 = vaddq_s16(accl0, o0); \
-                        accl1 = vaddq_s16(accl1, o1); \
-                        acch0 = vaddq_s16(acch0, o2); \
-                        acch1 = vaddq_s16(acch1, o3); \
+                        reconstruct_int16_pair(r0h_j0, r0l_j0, o0, o1); \
+                        acl0_j0 = vaddq_s16(acl0_j0, o0); ach0_j0 = vaddq_s16(ach0_j0, o1); \
+                        reconstruct_int16_pair(r1h_j0, r1l_j0, o2, o3); \
+                        acl1_j0 = vaddq_s16(acl1_j0, o2); ach1_j0 = vaddq_s16(ach1_j0, o3); \
+                        \
+                        reconstruct_int16_pair(r0h_j1, r0l_j1, o0, o1); \
+                        acl0_j1 = vaddq_s16(acl0_j1, o0); ach0_j1 = vaddq_s16(ach0_j1, o1); \
+                        reconstruct_int16_pair(r1h_j1, r1l_j1, o2, o3); \
+                        acl1_j1 = vaddq_s16(acl1_j1, o2); ach1_j1 = vaddq_s16(ach1_j1, o3); \
                     }
 
-                    PROCESS_32_ROWS(A + k * M / 2 + i_packed,      acc0, acc1, acc2, acc3);
-                    PROCESS_32_ROWS(A + k * M / 2 + i_packed + 16, acc4, acc5, acc6, acc7);
-#undef PROCESS_32_ROWS
+                    PROCESS_32_ROWS_2COL(A + k * M / 2 + i_packed,      \
+                                         acc0_j0, acc1_j0, acc2_j0, acc3_j0, \
+                                         acc0_j1, acc1_j1, acc2_j1, acc3_j1);
+                    PROCESS_32_ROWS_2COL(A + k * M / 2 + i_packed + 16, \
+                                         acc4_j0, acc5_j0, acc6_j0, acc7_j0, \
+                                         acc4_j1, acc5_j1, acc6_j1, acc7_j1);
+#undef PROCESS_32_ROWS_2COL
                 }
 
                 // Write-back with FMA
-                float32_t* pC = &(C[j * M + i]);
-#define WRITE_BACK(out_ptr, accl, acch) { \
-                    vst1q_f32(out_ptr + 0, vfmaq_f32(vld1q_f32(out_ptr + 0), vcvtq_f32_s32(vmovl_s16(vget_low_s16(accl))), v_rescale)); \
-                    vst1q_f32(out_ptr + 4, vfmaq_f32(vld1q_f32(out_ptr + 4), vcvtq_f32_s32(vmovl_s16(vget_high_s16(accl))), v_rescale)); \
-                    vst1q_f32(out_ptr + 8, vfmaq_f32(vld1q_f32(out_ptr + 8), vcvtq_f32_s32(vmovl_s16(vget_low_s16(acch))), v_rescale)); \
-                    vst1q_f32(out_ptr + 12, vfmaq_f32(vld1q_f32(out_ptr + 12), vcvtq_f32_s32(vmovl_s16(vget_high_s16(acch))), v_rescale)); \
+                float32_t* pC0 = &(C[j * M + i]);
+                float32_t* pC1 = &(C[(j + 1) * M + i]);
+
+#define WRITE_BACK(out_ptr, accl, acch, rescale) { \
+                    vst1q_f32(out_ptr + 0, vfmaq_f32(vld1q_f32(out_ptr + 0), vcvtq_f32_s32(vmovl_s16(vget_low_s16(accl))), rescale)); \
+                    vst1q_f32(out_ptr + 4, vfmaq_f32(vld1q_f32(out_ptr + 4), vcvtq_f32_s32(vmovl_s16(vget_high_s16(accl))), rescale)); \
+                    vst1q_f32(out_ptr + 8, vfmaq_f32(vld1q_f32(out_ptr + 8), vcvtq_f32_s32(vmovl_s16(vget_low_s16(acch))), rescale)); \
+                    vst1q_f32(out_ptr + 12, vfmaq_f32(vld1q_f32(out_ptr + 12), vcvtq_f32_s32(vmovl_s16(vget_high_s16(acch))), rescale)); \
                 }
-                WRITE_BACK(pC,      acc0, acc1);
-                WRITE_BACK(pC + 16, acc2, acc3);
-                WRITE_BACK(pC + 32, acc4, acc5);
-                WRITE_BACK(pC + 48, acc6, acc7);
+
+                WRITE_BACK(pC0,      acc0_j0, acc1_j0, v_rescale0);
+                WRITE_BACK(pC0 + 16, acc2_j0, acc3_j0, v_rescale0);
+                WRITE_BACK(pC0 + 32, acc4_j0, acc5_j0, v_rescale0);
+                WRITE_BACK(pC0 + 48, acc6_j0, acc7_j0, v_rescale0);
+
+                WRITE_BACK(pC1,      acc0_j1, acc1_j1, v_rescale1);
+                WRITE_BACK(pC1 + 16, acc2_j1, acc3_j1, v_rescale1);
+                WRITE_BACK(pC1 + 32, acc4_j1, acc5_j1, v_rescale1);
+                WRITE_BACK(pC1 + 48, acc6_j1, acc7_j1, v_rescale1);
 #undef WRITE_BACK
             }
         }
     }
+
+    aligned_free(QLUT0);
+    aligned_free(QLUT1);
+    aligned_free(LUT_Scales);
+}
 
     aligned_free(QLUT);
     aligned_free(LUT_Scales);
