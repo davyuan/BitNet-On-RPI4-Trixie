a32fe375a475121452f133b456c8b9edc348b275
David Yuan <davyuan@gmail.com>
2026-02-08 14:35:25 -0600

    update

--- include/bitnet-lut-kernels.h
+++ include/bitnet-lut-kernels.h
@@ -78,13 +78,11 @@ inline void reconstruct_int16_pair(int8x16_t high, int8x16_t low, int16x8_t& out
 }
 
 inline void reconstruct_int16_pair2(int8x16_t high, int8x16_t low, int16x8_t& out_lo, int16x8_t& out_hi) {
-    // Little-endian: [low_byte, high_byte] -> 16-bit word.
-    // vzip1q_u8 interleaves the bytes: low[0], high[0], low[1], high[1], ...
-    uint8x16_t res_lo = vzip1q_u8(vreinterpretq_u8_s8(low), vreinterpretq_u8_s8(high));
-    uint8x16_t res_hi = vzip2q_u8(vreinterpretq_u8_s8(low), vreinterpretq_u8_s8(high));
-    
-    out_lo = vreinterpretq_s16_u8(res_lo);
-    out_hi = vreinterpretq_s16_u8(res_hi);
+    // Interleave low and high bytes to form 16-bit words.
+    // In little-endian, low byte comes first [L0, H0, L1, H1...].
+    int8x16x2_t res = vzipq_s8(low, high);
+    out_lo = vreinterpretq_s16_s8(res.val[0]);
+    out_hi = vreinterpretq_s16_s8(res.val[1]);
 }
 #endif
 #endif
